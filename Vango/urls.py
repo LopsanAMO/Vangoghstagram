592b558c98146f51dbb52a9dbd72a920dd788e9f
LopsanAMO <makiki789@gmail.com>
2017-06-30 16:41:58 -0500

    mejorando la url

--- Vango/urls.py
+++ Vango/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from userprofiles import urls as Userurls
 from userprofiles.views import Home, logout_view
 
 
 urlpatterns = [
     url(r'^$', Home.as_view()),
     url('', include('social_django.urls', namespace='social')),
+    url(r'^user/', include(Userurls)),
     url(r'^admin/', admin.site.urls),
     url(r'^logout/', logout_view),
 ]
--- userprofiles/views.py
+++ userprofiles/views.py
@@ -1,12 +1,15 @@
 from django.conf import settings
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from django.views.generic import View
 from django.contrib.auth import logout
 from instagram.client import InstagramAPI
 from social_django.models import UserSocialAuth
 from .models import Vangouser
+import Algorithmia
+import json
 
 client_secret = settings.SOCIAL_AUTH_INSTAGRAM_SECRET
+client = Algorithmia.client("simCAFV0mJtYSMFC7g1kaOv8XGU1")
 
 
 def logout_view(request):
@@ -23,12 +26,31 @@ class Home(View):
             access_token = user.extra_data['access_token']
             uuid = user.uid
             api = InstagramAPI(access_token=access_token, client_secret=client_secret)
-            recent_media, next_ = api.user_recent_media(user_id=uuid, count=10)
+            recent_media, next_ = api.user_recent_media(user_id=uuid, count=7)
             for media in recent_media:
-               img.append(media.images['standard_resolution'].url)
+                img.append(media.images['standard_resolution'].url)
         except Exception:
             pass
         ctx = {
             'imgs': img
         }
         return render(request, template_name, ctx)
+
+
+def modified_photo(request):
+    if request.method == 'POST':
+        img_url = request.POST.get('img_url')
+        input_ = {
+            "images": [img_url],
+            "savePaths": ["data://lopsan/vango/vangoh.jpg"],
+            "filterName": "space_pizza"
+        }
+        nlp_directory = client.dir("data://lopsan/nlp_directory")
+        if nlp_directory.exists() is False:
+            nlp_directory.create()
+        result = client.algo("deeplearning/DeepFilter/").pipe(input_).result['savePaths'][0]
+        img_url = "https://algorithmia.com/v1/data/{}".format(result.split("data://")[1])
+        ctx = {
+            'img_url': img_url
+        }
+        return HttpResponse(json.dumps(ctx), content_type="application/json")
