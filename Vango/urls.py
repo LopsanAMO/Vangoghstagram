158b186f0bc1ac647d52d433fb846f2ec6b5e4f9
LopsanAMO <makiki789@gmail.com>
2017-06-30 01:28:04 -0500

    agragando url para hacer logout

--- Vango/urls.py
+++ Vango/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from userprofiles.views import Home
+from userprofiles.views import Home, logout_view
+
 
 urlpatterns = [
     url(r'^$', Home.as_view()),
     url('', include('social_django.urls', namespace='social')),
     url(r'^admin/', admin.site.urls),
+    url(r'^logout/', logout_view),
 ]
