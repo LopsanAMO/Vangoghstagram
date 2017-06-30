c9e0d68aa63dbf27b036f22d169f235c8ce12cb7
LopsanAMO <makiki789@gmail.com>
2017-06-30 16:42:32 -0500

    sobrepoiendo imagenes

--- static/js/main.js
+++ static/js/main.js
@@ -10,4 +10,36 @@ $(document).ready(function(){
             $('.letras').css("color", "#272822");
         }
     });
+
+    (function( $ ){
+       $.fn.get_csrf = function() {
+           var end, start;
+           start = document.cookie.indexOf('csrftoken=');
+           end = document.cookie.indexOf(";", start);
+           if (-1 === end) {
+             end = document.cookie.length;
+           }
+           csrftoken = document.cookie.substring(start, end).replace('csrftoken=', '');
+           return csrftoken;
+       };
+   })( $ );
+   var random = Math.floor(Math.random()*10);
+    $.ajax({
+            method: 'POST',
+            url: '/user/image/',
+            headers: {
+                'x-csrftoken': $().get_csrf()
+            },
+            data:{
+                img_url: $('.img_van').eq(random ? random > 0 : 1).attr('src')
+            },
+            success: function (data) {
+                console.log(data);
+                $(".img_van").eq(random ? random > 0 : 1).attr("src", data.img_url);
+            },
+            error: function(data, qXRH, status, throwStatus) {
+                console.log('holis, fallo algo');
+            }
+        });
+
 });
