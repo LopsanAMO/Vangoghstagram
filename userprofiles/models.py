f6ba088266b281c8c958d3f1c22c5d5f5aa2afe1
LopsanAMO <makiki789@gmail.com>
2017-06-30 08:24:33 -0500

    agregando modelo para guardar informacion de IG

--- userprofiles/models.py
+++ userprofiles/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from social_django.models import UserSocialAuth
 
-# Create your models here.
+
+class Vangouser(models.Model):
+    user = models.OneToOneField(User, blank=True, null=True)
+    username = models.CharField(blank=True, max_length=100)
+    profile_picture = models.CharField(blank=True, max_length=100)
+    followed_by = models.CharField(blank=True, max_length=100)
+    media = models.CharField(blank=True, max_length=100)
+    follows = models.CharField(blank=True, max_length=100)
+    bio = models.CharField(blank=True, max_length=500)
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        verbose_name = 'Informacion extra'
+        verbose_name_plural = 'Informacion extra'
